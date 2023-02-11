2df0f193c813a7698e8135217001c4e1284a01d7
Nakagawa Shinichi <98950032+shinpochi2002@users.noreply.github.com>
2023-02-11 17:52:38 +0900

    [update]bustimetableのswiftlintエラー修正
    swiftlintを導入して出てきたエラーを解消

--- BusdesKICNativeiOS/BusTimeTableView.swift
+++ BusdesKICNativeiOS/BusTimeTableView.swift
@@ -8,40 +8,38 @@
 import SwiftUI
 
 struct BusTimeTableView: View{
-    
-    @State var hour = "16時"//ここの時間の取り方がわからないので仮で設定
-    let time = ["16:03","17:03","17:30"]//とりあえずリストにしてます
-    let busgoal = ["パナソニック","パナソニック","パナソニック"]//timeと同様
-    
+    @State var hour = "16時"// ここの時間の取り方がわからないので仮で設定
+    let time = ["16:03", "17:03", "17:30"]// とりあえずリストにしてます
+    let busgoal = ["パナソニック", "パナソニック", "パナソニック"]// timeと同様
     var body: some View{
         List{
             Section{
-                ForEach(0 ..< busgoal.count){index in
+                ForEach(0 ..< busgoal.count, id: \.self){index in
                     HStack{
                         Text(time[index])
-                            .padding(.trailing,15)
-                            .font(.system(.caption,design: .monospaced))
+                            .padding(.trailing, 15)
+                            .font(.system(.caption, design: .monospaced))
                         Text(busgoal[index])
-                            .font(.system(.caption,design: .rounded))
+                            .font(.system(.caption, design: .rounded))
                     }
-                    .padding(.bottom,0)
-                    .padding(.top,0)
+                    .padding(.bottom, 0)
+                    .padding(.top, 0)
                 }
-                .padding(.leading,70)
-                .padding(.bottom,1)
-                .padding(.top,1)
+                .padding(.leading, 70)
+                .padding(.bottom, 1)
+                .padding(.top, 1)
                 .listRowSeparatorTint(Color.white)
             }header: {
                 Text(hour)
-                    .font(.system(.body,design: .rounded))
+                    .font(.system(.body, design: .rounded))
                     .foregroundColor(.black)
-                    .padding(.vertical,8)
-                    .padding(.leading,20)
+                    .padding(.vertical, 8)
+                    .padding(.leading, 20)
             }
         }
         .listStyle(.plain)
         .listSectionSeparator(.hidden)
-        .environment(\.defaultMinListRowHeight,25)
+        .environment(\.defaultMinListRowHeight, 25)
     }
 }
 struct BusTimeTableView_Previews: PreviewProvider{
