8e680c49da92330863c354a95d25b534ba691227
Nakagawa Shinichi <98950032+shinpochi2002@users.noreply.github.com>
2023-04-28 20:43:46 +0900

    [update]SettigViewの変更
    webviewを１つのページでするために

--- BusdesKICNativeiOS/SettingView.swift
+++ BusdesKICNativeiOS/SettingView.swift
@@ -1,40 +1,50 @@
 import SwiftUI
 
 struct Setttingview: View {
+    let campusbusschedule = "https://mercy34mercy.github.io/bustimer_kic/shuttle/schedule.jpg"
+    let  canpasbustimetable = "https://mercy34mercy.github.io/bustimer_kic/shuttle/timetable.jpg"
+    let terms = "https://mercy34mercy.github.io/bustimer_kic/policy/"
+    let feedback = "https://forms.gle/EkfH4jRu6PKhkEjPA"
+    let twitter = "https://twitter.com/busdes_kic"
     var body: some View {
         NavigationView{
             VStack{
                 Text("Busdes!!!!")
                 List{
                     Section{
-                        NavigationLink(destination: CampusBusScheduleView()){
+                        NavigationLink(destination: MainWebView(url: campusbusschedule)){
                             Text("運行スケジュール")
                         }
-                        NavigationLink(destination: CampusBusTimeTableView()){
+                        NavigationLink(destination: MainWebView(url: canpasbustimetable)){
                             Text("時刻表")
                         }
                     } header: {
                         Text("キャンパス間シャトル")
                     }
                     Section{
-                        NavigationLink(destination: TermsView()){
-                                Text("利用規約")
+                        NavigationLink(destination: MainWebView(url: terms)){
+                            Text("利用規約")
                         }
-                        NavigationLink(destination: FeedbackView()){
-                                Text("フィードバックを送信")
+                        NavigationLink(destination: MainWebView(url: feedback)){
+                            Text("フィードバックを送信")
                         }
-                        NavigationLink(destination: TwitterView()){
-                                Text("最新情報【Twitter】")
+                        NavigationLink(destination: MainWebView(url: twitter)) {
+                            Text("最新情報【Twitter】")
                         }
                     } header: {
                         Text("設定")
                     }
-                }
+                }.listStyle(.insetGrouped)
             }
-        }.navigationBarTitle(Text("Navigation"))
+        }
     }
 }
-
+struct MainWebView: View {
+    let url: String
+    var body: some View {
+        MyWebView(url: url)
+    }
+}
 struct Setttingview_Previews: PreviewProvider {
     static var previews: some View {
         Setttingview()
